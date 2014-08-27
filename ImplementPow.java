c288b953210236d7ab3e90d0f4298f0f39c1561f
Zhihao.Yang <1012421196@qq.com>
2014-08-28 00:12:19 +0800

    Create ImplementPow.java
    One possible solution of implementation of function pow

---
+++ ImplementPow.java
@@ -0,0 +1,26 @@
+public class ImplementPow {
+
+    public double pow(double x, int n) {
+    
+        // if n is zero, just return 1.0
+       if (n == 0) {
+			return 1.0;
+		}
+
+        // use binary recursion can reduce the number of computation 
+		double half = pow(x, n / 2);
+		
+		// being indivisible
+		if (n % 2 == 0) {
+			return half * half;
+		} 
+		// positive n
+		else if (n > 0) {
+			return half * half * x;
+		}
+		// negative n
+		else {
+			return half * half / x;
+		}
+    }
+}
